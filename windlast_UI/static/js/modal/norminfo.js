61a53f880b53d63da1e4163880fa7c6ebbc8cf1e
Jonas Schmid <crafter791@gmail.com>
2025-11-19 09:56:04 +0100

    Info für 13814

--- windlast_UI/static/js/modal/norminfo.js
+++ windlast_UI/static/js/modal/norminfo.js
@@ -3,27 +3,48 @@
 
 export const NORMINFO = {
   EN_13814_2005: {
-    title: "EN 13814:2005 – Fahrgeschäfte und Anlagen für Jahrmärkte",
+    title: "DIN EN 13814:2005-06 – Fliegende Bauten und Anlagen für Veranstaltungsplätze und Vergnügungsparks – Sicherheit",
     body: `
       <p>
-        Diese Norm regelt Sicherheit, Auslegung und Prüfung von Fahrgeschäften
-        und ähnlichen Anlagen.
+        Diese Norm regelt unter anderem die Windlastberechnung für fliegende Bauten. Dabei gibt sie Staudrücke und ein Verfahren zum Standsicherheitsnachweis vor, verweist zur Berechnung der Windkräfte jedoch auf die DIN EN 1991-1-4.
+        Sie ist in Deutschland baurechtlich eingeführt für die Berechnung von fliegenden Bauten. Neuere Versionen der Norm sind veröffentlicht, aber noch nicht baurechtlich eingeführt.
+        Staudrücke werden für Konstruktionen mit einer Höhe bis zu 50m vereinfacht in einer Tabelle vorgegeben. Daher ist die Norm nur unter bestimmten Voraussetzungen anwendbar:
       </p>
       <ul>
-        <li>Ausgabejahr: 2005</li>
-        <li>Geltungsbereich: Fliegende Bauten / Fahrgeschäfte</li>
-        <li>Hinweis: In einigen Ländern inzwischen durch EN 13814:2019 ersetzt,
-            bei Windlast 2 aktuell als Referenznorm 2005 hinterlegt.</li>
+        <li>Referenzwindgeschwindigkeit höchstens 28 m/s (Achtung bei Windzone 4 und in Küstengebieten)</li>
+        <li>Ab einer Windgeschwindigkeit von 25 m/s in 10 m Höhe muss die Anlage geschützt und angemessen verstärkt werden</li>
       </ul>
+      <p>
+        Für komplexere Bauwerke, höhere Windgeschwindigkeiten oder exponierte Standorte ist die Anwendung der DIN EN 13814:2005-06 nicht vorgesehen. Hier sind gesonderte Nachweise nach DIN EN 1991-1-4 erforderlich.
+      </p>
+      <p>
+        <b>Diese Software liefert einen solchen Nachweis auch bei der Berechnung nach DIN EN 1991-1-4 nicht. Wendet euch hier an einen Statiker.</b>
+      </p>
+      <p>
+        Es besteht die Möglichkeit, die angenommenen Staudrücke abzumindern, wenn bestimmte Schutzmaßnahmen möglich sind. Bei der vorliegenden Berechung wurden diese Abminderungen nicht berücksichtigt.
+        Bei nicht gewährleisteter Standsicherheit werden die Abminderungen automatisch in einer zweiten Berechnung berücksichtigt. Details hierzu findet im entsprechenden Abschnitt der Tabelle.
+      </p>
     `,
     alt: {
-      // Beispiel – Keys an deine data-szenario-Werte anpassen
       "IN_BETRIEB": {
-        title: "EN 13814:2005 – Alternative „Wind 0°“",
+        title: "DIN EN 13814:2005-06 – Berücksichtigung von möglichen Schutzmaßnahmen",
         body: `
           <p>
-            Diese Alternative bildet ein spezielles Nachweisszenario ab
-            (z.&nbsp;B. Windrichtung 0°, besondere Aufstellung, etc.).
+            Bei der Berechnung nach DIN EN 13814:2005-06 können Abminderungen der Staudrücke berücksichtigt werden, wenn bestimmte Schutzmaßnahmen möglich sind. Bei der Berechnung &bdquo;mit Schutzmaßnahmen&ldquo; wurden diese Abminderungen berücksichtigt.
+            Sollen die hier aufgelisteten Werte verwendet werden, müssen ab einer Windgeschwindigkeit von 15 m/s Schutzmaßnahmen getroffen werden.
+            In unseren Bereich sind zum Beispiel folgende Schutzmaßnahmen möglich:
+          </p>
+          <ul>
+            <li>Abnehmen von Planen oder anderen großen Windangriffsflächen</li>
+            <li>Evakuierung der Konstruktion und der umliegenden Bereiche</li>
+          </ul>
+          <p>
+            Die Schutzmaßnahmen müssen vorher definiert werden und wir müssen sicherstellen, dass diese Maßnahmen auch tatsächlich umgesetzt werden.
+            <b>Andernfalls dürfen die abgeminderten Staudrücke nicht verwendet werden.</b>
+          </p>
+          <p>
+            Wird als Schutzmaßnahme die Konstruktion verändert (z.&nbsp;B. Abnehmen von Planen), muss für die Konstruktion in diesem Zustand ebenfalls ein Standsicherheitsnachweis geführt werden.
+            <b>Dieser Nachweis wird von dieser Software nicht automatisch geführt.</b>
           </p>
         `
       },
@@ -31,7 +52,7 @@ export const NORMINFO = {
   },
 
   EN_17879_2024: {
-    title: "EN 17879:2024 – Temporäre Bauten / Konstruktionen",
+    title: "DIN EN 17879:2024-08 – Event-Strukturen – Sicherheit",
     body: `
       <p>
         Norm für temporäre Konstruktionen im Veranstaltungsbereich,
